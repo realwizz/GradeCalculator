de3346517e294e93ca25583cad056e2568030bac
OxWizz <123992662+realwizz@users.noreply.github.com>
2024-10-10 14:38:28 +0100

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,66 +8,98 @@ namespace GradeCalculator
 {
     internal class Program
     {
+        struct Module
+        {
+            public string moduleID;
+            public string moduleTitle;
+            public int moduleCredits;
+            public double mark;
+
+            public Module(string moduleid, string moduletitle, int modulecredits, double Mark)
+            {
+                moduleID = moduleid;
+                moduleTitle = moduletitle;
+                moduleCredits = modulecredits;
+                mark = Mark;
+            }
+        }
         static void Main(string[] args)
         {
-            //Declaring variables
-            string moduleID;
-            string moduleTitle;
-            int moduleCredits;
-            int moduleMark;
-            int averageMark;
-            int bestAverage;
-            char overallGrade;
+            const int numnberOfModules = 6;
+            Module[] modules = new Module[numnberOfModules];
 
-            //Read in data
-            Console.WriteLine("Input the module ID");
-            moduleID = Console.ReadLine();
-
-            Console.WriteLine("Input the module title");
-            moduleTitle = Console.ReadLine();
-
-            Console.WriteLine("Input the module credits");
-            moduleCredits = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Input the module mark");
-            moduleMark = Convert.ToInt32(Console.ReadLine());
-
-            averageMark = (moduleID / moduleCredits);
-
-
-            for (int i >= 70; i <= averageMark;)    
+            //Input module data
+            for (int i = 0; i < numnberOfModules; i++)
             {
-                overallGrade = "A"
+                Console.WriteLine("Enter module details {i + 1}:");
+                Console.Write("Module ID: ");
+                string moduleid = Console.ReadLine();
+
+                Console.Write("Module Title: ");
+                string moduletitle = Console.ReadLine();
+
+                Console.Write("Credits (20): ");
+                int modulecredits = 20; //Fixed credits
+
+                Console.Write("Mark: ");
+                double mark = double.Parse(Console.ReadLine());
+
+                modules[i] = new Module(moduleid, moduletitle, modulecredits, mark);
+                Console.WriteLine();
             }
 
-            for (int i >= 60; i <= averageMark;)    
+            double weightedAverage = CalculateWeightedAverage(modules);
+            double bestAverage = CalculateBestAverage(modules);
+
+            Console.WriteLine($"Weighted Average Mark: {weightedAverage:F2} - Classification: {GetClassification(weightedAverage)}");
+            Console.WriteLine($"Best Average Mark (after removing worst grade: {bestAverage:F2} - Classification: {GetClassification(bestAverage)}");
+        }
+
+        static double CalculateWeightedAverage(Module[] modules)
+        {
+            double totalWeightedMarks = 0;
+            int totalCredits = 0;
+
+            foreach (var module in modules)
             {
-                overallGrade = "B"
+                totalWeightedMarks += module.mark * module.moduleCredits;
+                totalCredits += module.moduleCredits;
             }
 
-            for (int i >= 50; i <= averageMark;)    
+            return totalCredits > 0 ? totalWeightedMarks / totalCredits : 0;
+        }
+
+        static double CalculateBestAverage(Module[] modules)
+        {
+            double minMark = double.MaxValue;
+            double totalMarks = 0;
+            int count = modules.Length;
+
+            foreach (var module in modules)
             {
-                overallGrade = "C"
+                totalMarks += module.mark;
+                if (module.mark < minMark)
+                {
+                    minMark = module.mark;
+                }
             }
 
-            for (int i >= 45; i <= averageMark;)    
-            {
-                overallGrade = "D"
-            }
-
-            for (int i >= 40; i <= averageMark;)    
-            {
-                overallGrade = "E"
-            }
-            for (int i < 40; i <= averageMark;)    
-            {
-                overallGrade = "F"
-            }
-
-            Console.WriteLine("Your Average Mark is " + averageMark + ", and your Overall Grade is " + overallGrade);
-
-            Console.WriteLine();
-            Console.ReadKey();
+            //Subtract the lowest mark and average the rest
+            return (totalMarks - minMark) / (count - 1);
+        }
+        static string GetClassification(double average)
+        {
+            if (average >= 70)
+                return "First Class";
+            else if (average >= 60)
+                return "Second Class Upper";
+            else if (average >= 50)
+                return "Second Class Lower";
+            else if (average >= 40)
+                return "Third Class";
+            else
+                return "Fail";
         }
     }
 }
+            
